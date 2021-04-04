fa360f54baf9c4ec6aa6faa96385006da40ee81c
3kingz <victorkenneth95@gmail.com>
2021-04-04 12:37:30 +0100

    code refactor

--- src/utils/generateColor.js
+++ src/utils/generateColor.js
@@ -1,29 +1,18 @@
 const { colors } = require("../config/colors");
 
 export const generateBgColor = (type) => {
-  if (type === "success") {
-    return colors.successOpacity;
-  } else if (type === "informational") {
-    return colors.informationalOpacity;
-  } else if (type === "warning") {
-    return colors.warningOpacity;
-  } else if (type === "danger") {
-    return colors.dangerOpacity;
-  } else {
+  const colorName = type + "Opacity";
+  const color = colors[colorName];
+  if (!color) {
     return null;
   }
+  return color;
 };
 
 export const generateBorderColor = (type) => {
-  if (type === "success") {
-    return colors.success;
-  } else if (type === "informational") {
-    return colors.informational;
-  } else if (type === "warning") {
-    return colors.warning;
-  } else if (type === "danger") {
-    return colors.danger;
-  } else {
+  const color = colors[type];
+  if (!color) {
     return null;
   }
+  return color;
 };
